5419dc79d2912fb1decec19215048ebd82280add
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-06-28 21:07:33 +0530

    UCIHAR Dataset uploaded

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,8 @@
+# 1. Fetching dataset from the Web and unzipping the file to a 
+# destination directory
 if(!(file.exists("../Course Project"))){
   dir.create("../Course Project")
 }
 datafile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(datafile, destfile = "../Course Project/UCIHARDataset.zip")
+unzip(zipfile = "../Course Project/UCIHARDataset.zip", exdir = "./UCIHARDataset")
